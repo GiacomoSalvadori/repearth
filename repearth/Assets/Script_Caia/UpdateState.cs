563d0f59d4bf98b91a1a3807230265e9babd539a
antoniocaia <48985071+antoniocaia@users.noreply.github.com>
2020-02-01 15:46:59 +0100

    Finished UpdateState

--- repearth/Assets/Script_Caia/UpdateState.cs
+++ repearth/Assets/Script_Caia/UpdateState.cs
@@ -5,25 +5,30 @@ using UnityEngine;
 public class UpdateState : MonoBehaviour
 {
     // 1 = nero; 2 = verde; 3 = grigio
-    public int color;
     public GameObject backNode;
     public GameObject nextNode;
 
+    private SpriteRenderer sprite;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        StartCoroutine(UpdateColor(sprite.color));
     }
 
-    void UpdateColor(int color)
+    IEnumerator UpdateColor(Color newColor)
     {
-        
+        sprite.color = newColor;
+
+        yield return new WaitForSeconds(4);
+
+        StartCoroutine(backNode.GetComponent<UpdateState>().UpdateColor(newColor));
+        StartCoroutine(nextNode.GetComponent<UpdateState>().UpdateColor(newColor));
     }
 }
