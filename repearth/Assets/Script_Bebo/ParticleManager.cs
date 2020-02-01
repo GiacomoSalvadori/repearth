98493e812859449ecfa2d98d991239e8ebc5368c
Luca Fagnoni <luca.fagno@hotmail.it>
2020-02-01 15:30:17 +0100

    [DEV] added getter/setter for particle color

---
+++ repearth/Assets/Script_Bebo/ParticleManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour
+{
+    private ParticleSystem.MainModule particleSys;
+
+    public Color Color
+    {
+        get => particleSys.startColor.color;
+        set
+        {
+            particleSys.startColor = value;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        particleSys = GetComponent<ParticleSystem>().main;
+    }
+}
