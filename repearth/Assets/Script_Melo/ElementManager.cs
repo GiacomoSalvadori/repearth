9acb113bdcd9aab77e8e91f4ad098a85a69ff69b
Carmelo Mammana <carmelomammana8@gmail.com>
2020-02-01 15:04:48 +0100

    Adding object around circle

---
+++ repearth/Assets/Script_Melo/ElementManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementManager : MonoBehaviour
+{
+    public float radius;
+
+    public GameObject prefab;
+    public int numberOfObjects = 20;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetPoints();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GetPoints()
+    {
+        for (int i = 0; i < numberOfObjects; i++)
+        {
+            float angle = i * Mathf.PI * 2 / numberOfObjects;
+            Vector3 pos = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) * radius;
+            Instantiate(prefab, pos, Quaternion.identity);
+        }
+    }
+}
