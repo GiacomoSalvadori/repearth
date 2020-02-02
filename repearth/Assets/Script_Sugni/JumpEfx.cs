b161e4153247ab18d02a63beb541f8fcd280348b
Giacomo Salvadori <giaco.salvadori@gmal.com>
2020-02-02 13:55:06 +0100

    [DESIGN] Added landpage efx

---
+++ repearth/Assets/Script_Sugni/JumpEfx.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class JumpEfx : MonoBehaviour
+{
+    #region Values
+    public float jumpForce = 1.0f;
+    public float jumpTime = 1.0f;
+    public RectTransform target;
+    RectTransform rt;
+    #endregion
+
+    private void Awake()
+    {
+        rt = GetComponent<RectTransform>();
+        Debug.Log("rect "+rt.position);
+    }
+
+    private void Start()
+    {
+        //rt.DOJumpAnchorPos(target.position, jumpForce, 1, jumpTime).SetLoops(-1, LoopType.Yoyo);
+        rt.DOAnchorPosY(180.0f, 1.0f, false).SetLoops(-1, LoopType.Yoyo);
+    }
+}
