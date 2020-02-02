7d7ebaa8bcf88d08909c13fd1cb5d5e966779e04
Luca Fagnoni <luca.fagno@hotmail.it>
2020-02-02 15:14:47 +0100

    [DEV] pause menu

--- repearth/Assets/Script_Sugni/GameFlowController.cs
+++ repearth/Assets/Script_Sugni/GameFlowController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class GameFlowController : MonoBehaviour
 {
@@ -13,10 +14,14 @@ public class GameFlowController : MonoBehaviour
     public FlowChange OnEndGame;
     public FlowChange OnLoading;
     private bool started;
+    [SerializeField] GameObject pauseOverlay;
+    //private CanvasGroup canvas;
     #endregion
 
     private void Awake()
     {
+        //canvas = pauseOverlay.GetComponent<CanvasGroup>();
+        //pauseOverlay.SetActive(false);
         SubscribeMeForEndgame();
         previousScene = -1;
         started = true;
@@ -99,5 +104,21 @@ public class GameFlowController : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void PauseGame()
+    {
+        //TODO: show pause menu
+        //DOVirtual.DelayedCall(0.5f, () => canvas.DOFade(0.0f, 1.5f).OnComplete(() => pauseOverlay.SetActive(true)));
+        Time.timeScale = 0f;
+        pauseOverlay.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        //TODO: hide pause menu
+        Time.timeScale = 1f;
+        //DOVirtual.DelayedCall(0.5f, () => canvas.DOFade(0.0f, 1.5f).OnComplete(() => pauseOverlay.SetActive(false)));
+        pauseOverlay.SetActive(false);
     }
 }
