0fba4d555e3b1bcd5c390b853cabdeb345f818a8
Carmelo Mammana <carmelomammana8@gmail.com>
2020-02-02 12:24:39 +0100

    [DEV] Add sound manager

---
+++ repearth/Assets/Script_Melo/SoundClass.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SoundClass
+{
+    public string clipName;
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(1f, 3f)]
+    public float pitch;
+    [HideInInspector]
+    public AudioSource source;
+
+    public bool loop;
+}
