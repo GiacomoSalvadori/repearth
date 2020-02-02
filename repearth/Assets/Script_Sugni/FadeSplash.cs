d7092eacf07a97b51c5aa45e2d6307481302954a
Giacomo Salvadori <giaco.salvadori@gmal.com>
2020-02-02 14:47:50 +0100

    [DESIGN] Added splash screen

---
+++ repearth/Assets/Script_Sugni/FadeSplash.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class FadeSplash : MonoBehaviour
+{
+    #region Values
+    public CanvasGroup panel;
+    #endregion
+
+    void Awake()
+    {
+        panel.alpha = 1.0f;
+        DOVirtual.DelayedCall(1.0f, () => panel.DOFade(0.0f, 1.5f).OnComplete(() => gameObject.SetActive(false)));
+    }
+}
